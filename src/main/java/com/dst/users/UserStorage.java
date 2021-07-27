f26373f1a97ae5be47c9a818964215f18774c267
Vlad Pr <priselkov@tm10.ru>
2021-07-27 14:49:32 +0500

    roles changed

--- src/main/java/com/dst/users/UserStorage.java
+++ src/main/java/com/dst/users/UserStorage.java
@@ -18,11 +18,11 @@ public class UserStorage {
         users.add(new UserDispatcher("disp3", "123"));
         users.add(new UserDispatcher("disp4", "123"));
         users.add(new UserDispatcher("disp5", "123"));
-        users.add(new UserDriver("dr1-1500", "123", WarehouseMessage.NewTask.Weight.KG_1500));
-        users.add(new UserDriver("dr2-1750", "123", WarehouseMessage.NewTask.Weight.KG_1750));
+        users.add(new UserDriver("dr1-1500", "123", WarehouseMessage.NewTask.Weight.KG_5000));
+        users.add(new UserDriver("dr2-1750", "123", WarehouseMessage.NewTask.Weight.KG_3000));
         users.add(new UserDriver("dr3-1750", "123", WarehouseMessage.NewTask.Weight.KG_1750));
-        users.add(new UserDriver("dr4-2000", "123", WarehouseMessage.NewTask.Weight.KG_2000));
-        users.add(new UserDriver("dr5-3000", "123", WarehouseMessage.NewTask.Weight.KG_3000));
+        users.add(new UserDriver("dr4-2000", "123", WarehouseMessage.NewTask.Weight.KG_1500));
+        users.add(new UserDriver("dr5-3000", "123", WarehouseMessage.NewTask.Weight.KG_2000));
         users.add(new UserDriver("dr6-5000", "123", WarehouseMessage.NewTask.Weight.KG_5000));
     }
 }
